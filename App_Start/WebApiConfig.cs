1134538fe4ce6c5c2073fbe514a88f871a7a1633
Denis K <den.kasakov@gmail.com>
2019-05-11 13:30:14 +0300

    Initial commit with ASP.NET MVC and Angular Skeleton

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace mvc_classic_angular
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/AngularDataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace mvc_classic_angular.Controllers
+{
+    public class AngularDataController : Controller
+    {
+        // GET: AngularData
+        [HttpGet()]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
